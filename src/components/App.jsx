e6de4d83b5c8ad716b145d26f89949b633a173fb
Darrick <dmckirnan@hotmail.com>
2017-12-17 11:53:19 -0800

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navbar from './Navbar.jsx';
+import Main from './Main.jsx';
+
+const App = () =>
+  (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  )
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import configureStore from './store/configureStore';
+import App from './components/App.jsx';
+
+const store = configureStore();
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-undef */
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <App />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
