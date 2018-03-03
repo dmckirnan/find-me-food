337145966a7396b91d834793804c83e3cfc4088b
Darrick <dmckirnan@hotmail.com>
2018-03-03 12:46:45 -0800

    trying

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ const config = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-    modules: ['node_modules'],
+    modules: [path.resolve(__dirname, '/src/components'), 'node_modules'],
     alias: {
       components: path.resolve(__dirname, 'src/components'),
     }
